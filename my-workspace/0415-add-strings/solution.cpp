3aae86d7a7c373f8ee4ae41672dace0119f88203
Tanish Solanki <solankitanish77@gmail.com>
2025-06-12 07:09:41 +0000

    Sync LeetCode submission Runtime - 23 ms (8.61%), Memory - 59.2 MB (5.14%)

---
+++ my-workspace/0415-add-strings/solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string add(string num1, string num2) {
+        string ans;
+        int index1 = num1.size() - 1;
+        int index2 = num2.size() - 1;
+        int carry = 0;
+        while (index2 >= 0) {
+            int sum = (num1[index1] - '0') + (num2[index2] - '0') + carry;
+            carry = sum / 10;
+            char c = '0' + (sum % 10);
+            ans = c + ans;
+            index1--;
+            index2--;
+        }
+        while (index1 >= 0) {
+            int sum = (num1[index1] - '0') + carry;
+            carry = sum / 10;
+            char c = '0' + (sum % 10);
+            ans = c + ans;
+            index1--;
+        }
+        if (carry > 0) {
+            ans = (char)('0' + carry) + ans;
+        }
+
+        return ans;
+    }
+
+    string addStrings(string num1, string num2) {
+        if (num1.size() < num2.size()) {
+            return add(num2, num1);
+        } else {
+            return add(num1, num2);
+        }
+    }
+};
+
