6855534dcf2cdd5f36e7c4ad394b4348bbf85cdc
Tanish Solanki <solankitanish77@gmail.com>
2025-09-18 18:49:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.3 MB (78.23%)

--- my-workspace/0074-search-a-2d-matrix/solution.cpp
+++ my-workspace/0074-search-a-2d-matrix/solution.cpp
@@ -3,23 +3,22 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size();
         int n=matrix[0].size();
-        int row_index;
-        int column_index;
         int start=0;
         int end=m*n-1;
         while(start<=end){
-           int mid=start+(end-start)/2;
-           row_index=mid/n;
-           column_index=mid%n;
-           if(matrix[row_index][column_index]==target){
-            return 1;
-           }
-           else if(matrix[row_index][column_index]<target){
-            start=mid+1;
-           }
-           else{
-            end=mid-1;
-           }
+            int mid=start+(end-start)/2;
+            int rowI=mid/n;
+            int columnI=mid%n;
+            if(matrix[rowI][columnI]==target){
+                return 1;
+            }
+            else if(matrix[rowI][columnI]<target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+
         }
         return 0;
     }
