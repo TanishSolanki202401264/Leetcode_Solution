4f38062a0fbd887c8b876e82e726c721aa075955
Tanish Solanki <solankitanish77@gmail.com>
2025-12-10 13:34:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (96.92%)

--- my-workspace/1960-check-if-the-sentence-is-pangram/solution.cpp
+++ my-workspace/1960-check-if-the-sentence-is-pangram/solution.cpp
@@ -6,8 +6,9 @@ public:
             alpha[sentence[i]-'a']=1;
         }
         for(int i=0;i<26;i++){
-            if(alpha[i]==0)
-            return 0;
+            if(alpha[i]==0){
+                return 0;
+            }
         }
         return 1;
     }
