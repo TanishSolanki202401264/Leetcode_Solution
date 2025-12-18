1277d0bc5af63dbb11be2a05ac25ad5959a69677
Tanish Solanki <solankitanish77@gmail.com>
2025-12-18 14:37:31 +0000

    Sync LeetCode submission Runtime - 3 ms (25.52%), Memory - 10.7 MB (49.67%)

---
+++ my-workspace/0046-permutations/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+   void permut(vector<int>&arr,vector<vector<int>>&ans,int index){
+      if(arr.size()==index){
+        ans.push_back(arr);
+        return ;
+      }
+      for(int i=index;i<arr.size();i++){
+        swap(arr[i],arr[index]);
+        permut(arr,ans,index+1);
+        swap(arr[i],arr[index]);
+      }
+   }
+      
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+      permut(nums,ans,0);
+         return ans;
+    }
+};
