d3e2219b86325251d88a516bdd13d10332dc6c64
Tanish Solanki <solankitanish77@gmail.com>
2026-02-18 18:38:12 +0000

    Sync LeetCode submission Runtime - 8 ms (54.51%), Memory - 14.7 MB (30.99%)

---
+++ my-workspace/1371-minimum-remove-to-make-valid-parentheses/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+
+        string temp;
+        int open = 0;
+
+        // remove extra ')'
+        for(char c : s) {
+            if(c == '(') {
+                open++;
+                temp.push_back(c);
+            }
+            else if(c == ')') {
+                if(open > 0) {
+                    open--;
+                    temp.push_back(c);
+                }
+            }
+            else {
+                temp.push_back(c);
+            }
+        }
+
+        // remove extra '(' from right
+        string result;
+        for(int i = temp.size() - 1; i >= 0; i--) {
+            if(temp[i] == '(' && open > 0) {
+                open--;
+                continue;
+            }
+            result.push_back(temp[i]);
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
