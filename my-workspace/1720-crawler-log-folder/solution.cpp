7b7acd1e1f56a128652e3e0dd7e1ad0c46e6bd1a
Tanish Solanki <solankitanish77@gmail.com>
2026-02-18 14:25:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.4 MB (36.37%)

---
+++ my-workspace/1720-crawler-log-folder/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        stack<string>st;
+        for(int i=0;i<logs.size();i++){
+                if(logs[i]!="../"  && logs[i]!="./"){
+                    st.push(logs[i]);
+                }
+                else if(logs[i]=="../"){
+                    if(!st.empty()){
+                    st.pop();}
+                }
+                else{
+                    continue;
+                }
+        }
+        return st.size();
+    }
+};
