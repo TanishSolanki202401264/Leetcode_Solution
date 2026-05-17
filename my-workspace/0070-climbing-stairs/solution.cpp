b3bc27aa73ad5ec58e0fb1bd35a330f895d80077
Tanish Solanki <solankitanish77@gmail.com>
2026-05-17 06:01:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (37.59%)

--- my-workspace/0070-climbing-stairs/solution.cpp
+++ my-workspace/0070-climbing-stairs/solution.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<=3){
-            return n;
-        }
-          vector<int> dp(n+1);
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-
-        for (int i = 4; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+        vector<int>dp(n+1);
+        dp[0]=1;
+       dp[1]=1;
+       for(int i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
+       }
+       return dp[n];
     }
 };
