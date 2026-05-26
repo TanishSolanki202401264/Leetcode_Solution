457d3e83d6b69d793b35b3e66a33c0027ccc56fe
Tanish Solanki <solankitanish77@gmail.com>
2026-05-26 14:25:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.9 MB (83.39%)

---
+++ my-workspace/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+   long aheadbuy,aheadnotbuy,currbuy,currnotbuy;
+   aheadbuy=aheadnotbuy=0;
+
+for(int idx=n-1;idx>=0;idx--){
+    
+    
+  currbuy= max(-prices[idx] +aheadnotbuy,aheadbuy);
+
+
+    currnotbuy= max(prices[idx]+aheadbuy,aheadnotbuy);
+   
+    
+    aheadbuy=currbuy;
+    aheadnotbuy=currnotbuy;
+}
+       return aheadbuy;
+    }
+};
