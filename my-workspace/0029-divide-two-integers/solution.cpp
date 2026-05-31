56805b94185fd5d5c4846ff5cda0072cc50f5939
Tanish Solanki <solankitanish77@gmail.com>
2026-05-31 10:02:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.8 MB (3.55%)

--- my-workspace/0029-divide-two-integers/solution.cpp
+++ my-workspace/0029-divide-two-integers/solution.cpp
@@ -1,10 +1,76 @@
+/*class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if(dividend==divisor){
+            return 1;
+        }
+        bool sign =true;
+        if(dividend >=0 && divisor<0){
+            sign =false;
+        }
+        else if(dividend<=0 && divisor>0){
+            sign =false;
+        }
+        long n=abs(dividend);
+        long d=abs(divisor);
+
+        long q=0;
+        while(n>=d){
+            int cnt=0;
+            while(n>=(d<<(cnt+1))){
+                cnt++;
+            }
+            q=q+(1<<cnt);
+            n=n-(d<<cnt);
+
+        }
+
+        if(q==(1<<31) && sign){
+            return INT_MAX;
+        }
+        if(q==(1<<31) && !sign){
+            return INT_MIN;
+        }
+        return sign?q:-q;
+    }
+};*/
 class Solution {
 public:
-    int divide(int dividend, int divisor) { 
-        if (dividend == INT_MIN && divisor == -1)
-            return INT_MAX; 
+    int divide(int dividend, int divisor) {
+        if(dividend == divisor) return 1;
 
-        return dividend / divisor;
+        if(dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
+
+        bool sign = true;
+
+        if((dividend < 0 && divisor > 0) || 
+           (dividend > 0 && divisor < 0)) {
+            sign = false;
+        }
+
+        long long n = llabs((long long)dividend);
+        long long d = llabs((long long)divisor);
+
+        long long q = 0;
+
+        while(n >= d) {
+            int cnt = 0;
+
+            while(n >= (d << (cnt + 1))) {
+                cnt++;
+            }
+
+            q += (1LL << cnt);
+            n -= (d << cnt);
+        }
+
+        if(sign) {
+            if(q > INT_MAX) return INT_MAX;
+            return (int)q;
+        } else {
+            if(-q < INT_MIN) return INT_MIN;
+            return (int)(-q);
+        }
     }
 };
-
