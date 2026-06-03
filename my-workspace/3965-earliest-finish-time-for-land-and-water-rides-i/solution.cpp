10cc9b44b66d08257fa9502487f0f4d8ef7a4608
Tanish Solanki <solankitanish77@gmail.com>
2026-06-03 19:25:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 91.9 MB (19.46%)

--- my-workspace/3965-earliest-finish-time-for-land-and-water-rides-i/solution.cpp
+++ my-workspace/3965-earliest-finish-time-for-land-and-water-rides-i/solution.cpp
@@ -1,43 +1,24 @@
 class Solution {
 public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-       /* int n=landStartTime.size();
-        int m=landDuration.size();
-
-        int mini=INT_MAX;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-   ans=ans+(landStartTime[i]+landDuration[i] +waterStartTime[j]+waterDuration[j]);
-   mini=min(mini,ans);
-            }
-        }  aa code jo land and water vala banne event independent hoy tyare j use karvano rehse
-        return ans;*/
-
-
-
-
-        int n=landStartTime.size();
+         int n=landStartTime.size();
         int m=waterStartTime.size();
 
-        int mini=INT_MAX;
-        int ans1=0;
-        int ans2=0;
-        for(int i=0;i<n;i++){
-   for(int j=0;j<m;j++){
-    //pahela land bharay jay then water bharay chee te case
-            int landcomplete=landStartTime[i]+landDuration[i];
-            int waterstarted=max(landcomplete,waterStartTime[j]);
-            int ans1=waterstarted+waterDuration[j];
-            //pahela water bharay jay thyen land bharay
+       int land=INT_MAX;
+       int water=INT_MAX;
+      int land_water=INT_MAX;
+      int water_land=INT_MAX;
+      for(int i=0;i<n;i++){
+        land=min(land,landStartTime[i]+landDuration[i]);
+      }
 
-            int watercomplete=waterStartTime[j]+waterDuration[j];
-            int landstarted=max(watercomplete,landStartTime[i]);
-            int ans2=landstarted+landDuration[i];
-
-            mini=min({mini,ans1,ans2});
-         }
-        }
-       return mini;
+    for(int i=0;i<m;i++){
+        water=min(water,waterStartTime[i]+waterDuration[i]);
+        land_water=min(land_water,max(waterStartTime[i],land) +waterDuration[i] );
+    }
+    for(int i=0;i<n;i++){
+        water_land=min(water_land,max(landStartTime[i],water) + landDuration[i]);
+    }
+     return min(water_land,land_water);
     }
 };
