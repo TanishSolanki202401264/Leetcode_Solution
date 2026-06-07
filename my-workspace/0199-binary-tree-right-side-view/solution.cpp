84f6d94e9bb48b48e485592f640410ad8e0fe81b
Tanish Solanki <solankitanish77@gmail.com>
2026-06-07 12:14:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.9 MB (85.53%)

--- my-workspace/0199-binary-tree-right-side-view/solution.cpp
+++ my-workspace/0199-binary-tree-right-side-view/solution.cpp
@@ -38,23 +38,25 @@ public:
 };*/
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) {
-        vector<int> ans;
-        if (root == NULL) return ans;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                TreeNode* temp = q.front();
-                q.pop();
-                if (i == n - 1) {
-                    ans.push_back(temp->val);
-                }
-                if (temp->left) q.push(temp->left);
-                if (temp->right) q.push(temp->right);
-            }
+
+    void solve(TreeNode* root, int level, vector<int>& ans) {
+        if(root == NULL) {
+            return;
         }
+
+        if(level == ans.size()) {
+            ans.push_back(root->val);
+        }
+
+        solve(root->right, level + 1, ans);
+        solve(root->left, level + 1, ans);
+    }
+
+    vector<int>rightSideView(TreeNode *root) {
+        vector<int> ans;
+
+        solve(root, 0, ans);
+
         return ans;
     }
 };
