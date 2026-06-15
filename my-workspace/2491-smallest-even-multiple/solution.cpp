1e24f0ae1b069ee5bd2e64fbd1502e5587bf9266
Tanish Solanki <solankitanish77@gmail.com>
2026-06-15 20:15:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8 MB (0.52%)

---
+++ my-workspace/2491-smallest-even-multiple/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        int ans=0;
+        for(int i=1;i<=2*n;i++){
+             if(i%2==0 && i%n==0){
+                ans=i;
+                break;
+             }
+        }
+        return ans;
+    }
+};
