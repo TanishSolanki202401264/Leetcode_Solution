b72b205ab6ad2b5c367eaff705d4775d2a0e562c
Tanish Solanki <solankitanish77@gmail.com>
2026-06-22 11:35:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (46.08%)

--- my-workspace/0081-search-in-rotated-sorted-array-ii/solution.cpp
+++ my-workspace/0081-search-in-rotated-sorted-array-ii/solution.cpp
@@ -1,12 +1,48 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                return 1;
+        int start = 0;
+        int end = nums.size() - 1;
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target)
+                return true;
+
+            // duplicates
+            if (nums[start] == nums[mid] &&
+                nums[mid] == nums[end]) {
+                start++;
+                end--;
+            }
+
+            // left half sorted
+            else if (nums[start] <= nums[mid]) {
+
+                if (nums[start] <= target &&
+                    target < nums[mid]) {
+                    end = mid - 1;
+                }
+                else {
+                    start = mid + 1;
+                }
+            }
+
+            // right half sorted
+            else {
+
+                if (nums[mid] < target &&
+                    target <= nums[end]) {
+                    start = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
             }
         }
-        return 0;
+
+        return false;
     }
 };
