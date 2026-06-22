8dde1dac6cb145bc6e43de8835b30e6e76c77a37
Tanish Solanki <solankitanish77@gmail.com>
2026-06-22 09:48:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.5 MB (53.16%)

--- my-workspace/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ my-workspace/0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int start=0;
-        int end=nums.size()-1;
-        int first=-1;
-        int last=-1;
-        int mid;
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                first=mid;
-                end=mid-1;
+        int n = nums.size();
+        int start = 0, end = n - 1;
+        int a1 = -1;
+        while (start <= end) {
+            int mid =start+(end-start)/2;
+            if (nums[mid] ==target) {
+                a1 = mid;
+                end = mid - 1;
             }
-            else if(nums[mid]<target){
-                start=mid+1;
+            else if (nums[mid] < target) {
+                start = mid + 1;
             }
-            else{
-                end=mid-1;
+            else {
+                end = mid - 1;
             }
         }
-        start=0;
-        end=nums.size()-1;
-         while(start<=end){
-            mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                last=mid;
-                start=mid+1;
+        start = 0;
+        end = n - 1;
+        int a2 = -1;
+        while (start <= end) {
+            int mid = start +(end-start)/ 2;
+            if (nums[mid] == target) {
+                a2 = mid;
+                start = mid + 1;
             }
-            else if(nums[mid]<target){
-                start=mid+1;
+            else if (nums[mid] < target) {
+                start = mid + 1;
             }
-            else{
-                end=mid-1;
+            else {
+                end = mid - 1;
             }
         }
-        vector<int>arr(2);
-        arr[0]=first;
-        arr[1]=last;
-        return arr;
+        return {a1, a2};
     }
 };
