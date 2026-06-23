d24e080db27e10b6280e17a468c1dce6c03ee899
Tanish Solanki <solankitanish77@gmail.com>
2026-06-23 19:56:22 +0000

    Sync LeetCode submission Runtime - 25 ms (5.01%), Memory - 67.1 MB (5.01%)

---
+++ my-workspace/1078-remove-outermost-parentheses/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans="";
+        int count=0;
+        for(char ch:s){
+            if(ch==')'){
+                count--;
+            }
+            if(count>0){
+                ans=ans+ch;
+            }
+            if(ch=='('){
+                count++;
+            }
+        }
+        return ans;
+    }
+};
