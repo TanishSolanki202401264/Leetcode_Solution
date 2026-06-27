1527423913dee9b1f0cd9445f6126ce96eb10a9d
Tanish Solanki <solankitanish77@gmail.com>
2026-06-27 14:02:28 +0000

    Sync LeetCode submission Runtime - 3 ms (50.94%), Memory - 14.1 MB (75.91%)

---
+++ my-workspace/0039-combination-sum/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void solve(int idx, vector<int>& candidates,
+               int target,
+               vector<int>& curr,
+               vector<vector<int>>& ans) {
+        if (target == 0) {
+            ans.push_back(curr);
+            return;
+        }
+        if (idx == candidates.size())
+            return;
+        if (candidates[idx]<=target) {
+            curr.push_back(candidates[idx]);
+            //ahiya number repeat karii shakay chee atle j idx as it is j rakhavano
+            solve(idx,candidates,
+                  target-candidates[idx],
+                  curr, ans);
+            curr.pop_back();
+        }
+        solve(idx + 1, candidates,
+              target, curr, ans);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates,
+                                       int target) {
+        vector<vector<int>>ans;
+        vector<int>curr;
+        solve(0,candidates,target,
+              curr, ans);
+        return ans;
+    }
+};
