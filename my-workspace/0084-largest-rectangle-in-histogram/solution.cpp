fc6af61532293a441a08c620566a6552c17c79ac
Tanish Solanki <solankitanish77@gmail.com>
2026-06-30 10:21:06 +0000

    Sync LeetCode submission Runtime - 8 ms (97.18%), Memory - 79.3 MB (98.82%)

--- my-workspace/0084-largest-rectangle-in-histogram/solution.cpp
+++ my-workspace/0084-largest-rectangle-in-histogram/solution.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        //Note:fiest find next smallest on right then find next smallest of left and width will be NSR-NSL-1 and area will be height *width atle pachii temathii maximum area find kari shakvi
-          int n=heights.size();
-        vector<int>right(n);
-        vector<int>left(n);
-        stack<int>st;
-        //NSR
-      
-        for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()]>heights[i]){
-                right[st.top()]=i;
-                st.pop();
-            }
-            st.push(i);
-        } //koi mate NSR jo na male to
-        while(!st.empty()){
-           right[st.top()]=n;
-           st.pop();
-        }     
-        //NSL
 
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty()&& heights[st.top()]>heights[i]){
-                left[st.top()]=i;
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int ans = 0;
+        for(int i = 0; i <= n; i++){
+            while(!st.empty() &&
+                 (i==n || heights[st.top()]>=heights[i])){
+                int height = heights[st.top()];
                 st.pop();
+                int width;
+                if(st.empty())
+                    width = i;
+                else
+                    width = i-st.top()-1;
+                ans = max(ans,height*width);
             }
             st.push(i);
-        }
-        while(!st.empty()){
-            left[st.top()]=-1;
-            st.pop();
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans=max(ans,heights[i]*(right[i]-left[i]-1));
         }
         return ans;
     }
-};auto it = atexit([ ]( ) { ofstream("display_runtime.txt") << "0";});
+};
