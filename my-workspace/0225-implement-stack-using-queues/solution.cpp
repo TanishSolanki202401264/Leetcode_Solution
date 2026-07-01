b463bda07d95cc48f7760f39f84f69d4262fc9f4
Tanish Solanki <solankitanish77@gmail.com>
2026-07-01 10:00:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (99.55%)

--- my-workspace/0225-implement-stack-using-queues/solution.cpp
+++ my-workspace/0225-implement-stack-using-queues/solution.cpp
@@ -1,62 +1,36 @@
 class MyStack {
 public:
-    queue<int> q1, q2;
+
+    queue<int> q;
 
     MyStack() {}
 
-    bool empty() {
-        return q1.empty() && q2.empty();
-    }
-
     void push(int x) {
-        if (!q1.empty())
-            q1.push(x);
-        else
-            q2.push(x);
-    }
 
-    int pop() {
-        if (empty()) return -1;
+        q.push(x);
 
-        if (!q1.empty()) {
-            while (q1.size() > 1) {
-                q2.push(q1.front());
-                q1.pop();
-            }
-            int ans = q1.front();
-            q1.pop();
-            return ans;
-        } else {
-            while (q2.size() > 1) {
-                q1.push(q2.front());
-                q2.pop();
-            }
-            int ans = q2.front();
-            q2.pop();
-            return ans;
+        int sz = q.size();
+
+        while (sz > 1) {
+            q.push(q.front());
+            q.pop();
+            sz--;
         }
     }
 
-    int top() {
-        if (empty()) return -1;
+    int pop() {
 
-        if (!q1.empty())
-            return q1.back();
-        else
-            return q2.back();
+        int ans = q.front();
+        q.pop();
+
+        return ans;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
     }
 };
-
-
-
-
-
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack* obj = new MyStack();
- * obj->push(x);
- * int param_2 = obj->pop();
- * int param_3 = obj->top();
- * bool param_4 = obj->empty();
- */
