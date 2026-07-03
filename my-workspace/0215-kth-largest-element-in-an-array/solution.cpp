ebb2440e354cce4b6c59c3de75872f2fc97b7a69
Tanish Solanki <solankitanish77@gmail.com>
2026-07-03 18:11:59 +0000

    Sync LeetCode submission Runtime - 27 ms (78.48%), Memory - 69.1 MB (91.66%)

--- my-workspace/0215-kth-largest-element-in-an-array/solution.cpp
+++ my-workspace/0215-kth-largest-element-in-an-array/solution.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-      
-     //sauthii pahela k th largest find karvo hoy to min heap and k th smallest find karvo hoy to max heap
-     priority_queue<int,vector<int>,greater<int>>p;
-     for(int i=0;i<k;i++){
-   p.push(nums[i]);
-     }
-     //have k thii bakina mate jo queue na sauthii nana element karta moto koi element male to tene q ma insert kari do
-     for(int i=k;i<nums.size();i++){
-        if(nums[i]>p.top()){
-            p.push(nums[i]);
-            p.pop();
-        }
-     }
-     return p.top();
+         //THIS IS ONLY FOR FUN PURPOSE SO DO NOT JUDGE
+         sort(nums.begin(),nums.end());
+         reverse(nums.begin(),nums.end());
+         return nums[k-1];
     }
 };
