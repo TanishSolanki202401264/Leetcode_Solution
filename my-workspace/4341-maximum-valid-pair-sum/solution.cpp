5b5894ebf97e25d55be7f25a0ef0366abce767bd
Tanish Solanki <solankitanish77@gmail.com>
2026-07-04 16:26:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 205.7 MB (60.76%)

---
+++ my-workspace/4341-maximum-valid-pair-sum/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxValidPairSum(vector<int>& nums, int k) {
+        int n=nums.size();
+        int maxi=INT_MIN;
+        int ans=INT_MIN;
+        for(int i=k;i<n;i++){
+            ans=max(ans,nums[i-k]);
+            maxi=max(maxi,ans+nums[i]);
+        }
+       
+        return maxi;
+    }
+};
