4c6e724d2d4778aaf9401f7360d41f63efe5b422
Tanish Solanki <solankitanish77@gmail.com>
2026-07-05 18:42:40 +0000

    Sync LeetCode submission Runtime - 13 ms (88.00%), Memory - 91.8 MB (35.70%)

--- my-workspace/0164-maximum-gap/solution.cpp
+++ my-workspace/0164-maximum-gap/solution.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        long long n=nums.size();
-        if(n<2){
+
+        int n = nums.size();
+
+        if(n < 2)
             return 0;
+        int mini = *min_element(nums.begin(), nums.end());
+        int maxi = *max_element(nums.begin(), nums.end());
+        if(mini == maxi)
+            return 0;
+        int gap = ceil((double)(maxi - mini) / (n - 1));
+        int bucketCount = (maxi - mini) / gap + 1;
+        vector<int> bucketMin(bucketCount, INT_MAX);
+        vector<int> bucketMax(bucketCount, INT_MIN);
+        vector<bool> used(bucketCount, false);
+        for(int x : nums) {
+            int idx = (x - mini) / gap;
+            bucketMin[idx] = min(bucketMin[idx], x);
+            bucketMax[idx] = max(bucketMax[idx], x);
+            used[idx] = true;
         }
-       
-        sort(nums.begin(),nums.end());
-        long long maxi=LLONG_MIN;
-        for(long long i=1;i<=n-1;i++){
-           maxi=max(maxi,(long long)nums[i]-nums[i-1]);
+        int ans = 0;
+        int prevMax = mini;
+        for(int i = 0; i < bucketCount; i++) {
+            if(!used[i])
+                continue;
+            ans = max(ans, bucketMin[i] - prevMax);
+            prevMax = bucketMax[i];
         }
-        return maxi;
+        return ans;
     }
 };
