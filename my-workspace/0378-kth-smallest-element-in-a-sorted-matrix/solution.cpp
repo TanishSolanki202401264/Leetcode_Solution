760c823b2a7956a7c3dbdd9a8750e680740dc75d
Tanish Solanki <solankitanish77@gmail.com>
2026-07-05 14:07:39 +0000

    Sync LeetCode submission Runtime - 7 ms (43.39%), Memory - 17.2 MB (54.57%)

--- my-workspace/0378-kth-smallest-element-in-a-sorted-matrix/solution.cpp
+++ my-workspace/0378-kth-smallest-element-in-a-sorted-matrix/solution.cpp
@@ -2,29 +2,23 @@ class Solution {
 public:
     int kthSmallest(vector<vector<int>>& matrix, int k) {
                 int n = matrix.size();
-
         // Min heap: {value, {row, col}}
         priority_queue<
             pair<int, pair<int,int>>,
             vector<pair<int, pair<int,int>>>,
             greater<pair<int, pair<int,int>>>
         > pq;
-
         // Push first element of each row
         for (int i = 0; i < n; i++) {
             pq.push({matrix[i][0], {i, 0}});
         }
-
         int ans = 0;
-
         while (k--) {
             auto element = pq.top();
             pq.pop();
-
             ans = element.first;
             int row = element.second.first;
             int col = element.second.second;
-
             // push next element in same row
             if (col + 1 < n) {
                 pq.push({matrix[row][col + 1], {row, col + 1}});
