7dea63d6125f5d710948ec1567db153aa4ce9e7d
Tanish Solanki <solankitanish77@gmail.com>
2026-07-10 20:33:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (31.49%)

--- my-workspace/0957-minimum-add-to-make-parentheses-valid/solution.cpp
+++ my-workspace/0957-minimum-add-to-make-parentheses-valid/solution.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        //without stack
-        int count=0;
-        stack<char>st;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                st.push(s[i]);
-            }
-            else{
-                if(st.empty()){
-                    count++;
-                }
-                else{
-                    st.pop();
-                }
-            }
-        }
-        return count+st.size();
+      // stack na case ma TC and SC ae banne O(N) thasti hati tene reduce kareva mate apde aa code ne without using stack pan karii j shakay chee atla mate j apde kahii shakay ke Open and count no use karta 
+       int open=0;
+       int count=0;
+       int n=s.size();
+       for(int i=0;i<n;i++){
+         if(s[i]=='('){
+            open++;
+         }
+         else{
+           if(open>0){
+            open--;
+           }
+           else{
+            count++;
+           }
+         }
+       }
+       return open+count;
     }
 };
