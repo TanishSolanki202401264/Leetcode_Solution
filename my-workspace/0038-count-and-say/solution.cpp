24f11f47aee42a1196b7035b5ec83c99f9a39fe5
Tanish Solanki <solankitanish77@gmail.com>
2026-07-10 21:27:54 +0000

    Sync LeetCode submission Runtime - 3 ms (81.07%), Memory - 9.9 MB (60.37%)

---
+++ my-workspace/0038-count-and-say/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+    string countAndSay(int n) {
+        string curr = "1";
+        for(int i = 2; i <= n; i++) {
+            string next = "";
+            int count = 1;
+            for(int j = 1; j < curr.size(); j++) {
+
+                if(curr[j] == curr[j-1]) {
+                    count++;
+                }
+                else {
+                    next += to_string(count);
+                    next += curr[j-1];
+                    count = 1;
+                }
+            }
+            next += to_string(count);
+            next += curr.back();
+            curr = next;
+        }
+        return curr;
+    }
+};
