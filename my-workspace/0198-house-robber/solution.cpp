0d4914b1188882c4f3b06f81e2d048cc9df47f2d
Tanish Solanki <solankitanish77@gmail.com>
2026-07-15 11:34:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.8 MB (6.15%)

--- my-workspace/0198-house-robber/solution.cpp
+++ my-workspace/0198-house-robber/solution.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
+     int f(int idx,vector<int>&nums,vector<int>&dp){
+        if(idx==0){
+            return nums[idx];
+        }
+        if(idx<0){
+            return 0;
+        }
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+     int pick=nums[idx]+f(idx-2,nums,dp);
+     int notpick=f(idx-1,nums,dp);
+     return dp[idx]= max(pick,notpick);
 
-
-
-
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        //aa curr,prev and prev2 valu logic will be the best because space optimize karine apee chee bahu vadhare padtii
-
-      
-        int prev=nums[0];
-        int prev2=0;
-        for(int i=1;i<n;i++){
-            int take=nums[i];
-            if(i>1){
-                take=take+prev2;
-        }
-        int nottake=0+prev;
-
-        int curri=max(take,nottake);
-        prev2=prev;
-        prev=curri;
+        vector<int>dp(n,-1);
+       return  f(n-1,nums,dp);
     }
-    return prev;}
 };
