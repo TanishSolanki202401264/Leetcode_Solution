f60eec6e40ff1ae1834882fcd6fc7a36f728a2b2
Tanish Solanki <solankitanish77@gmail.com>
2026-07-18 23:07:14 +0000

    Sync LeetCode submission Runtime - 39 ms (74.26%), Memory - 54.5 MB (22.92%)

--- my-workspace/0044-wildcard-matching/solution.cpp
+++ my-workspace/0044-wildcard-matching/solution.cpp
@@ -33,60 +33,39 @@ public:
 };*/
 //ama text and pattern undhii thai gayi chee s ae text chee and p ae pattern thase
 
-
 class Solution {
 public:
-    bool isMatch(string s, string p) {
+    int f(int i, int j, string &s, string &p,vector<vector<int>>&dp) {
 
+        if (i < 0 && j < 0) {
+            return 1;
+        }
+        if (j < 0 && i >= 0) {
+            return 0;
+        }
+        if (i < 0 && j >= 0) {
+            for (int jj = 0; jj <= j; jj++) {
+                if (p[jj] != '*') {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+       if(dp[i][j]!=-1){
+        return dp[i][j];
+       }
+        if (s[i] == p[j] || p[j] == '?') {
+            return dp[i][j]= f(i - 1, j - 1, s, p,dp);
+        }
+        if (p[j] == '*') {
+            return dp[i][j]= f(i - 1, j, s, p,dp) || f(i, j - 1, s, p,dp);
+        }
+        return 0;
+    }
+    bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-
-        vector<vector<bool>> dp(n + 1, vector<bool>(m + 1, false));
-
-        // both exhausted
-        dp[0][0] = true;
-
-        // pattern empty but string not empty
-        for (int i = 1; i <= n; i++) {
-            dp[i][0] = false;
-        }
-
-        // string empty but pattern may contain *
-        for (int j = 1; j <= m; j++) {
-
-            bool flag = true;
-
-            for (int k = 1; k <= j; k++) {
-                if (p[k - 1] != '*') {
-                    flag = false;
-                    break;
-                }
-            }
-
-            dp[0][j] = flag;
-        }
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 1; j <= m; j++) {
-
-                // character match or '?'
-                if (s[i - 1] == p[j - 1] || p[j - 1] == '?') {
-
-                    dp[i][j] = dp[i - 1][j - 1];
-                }
-
-                // '*'
-                else if (p[j - 1] == '*') {
-
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-                }
-
-                else {
-                    dp[i][j] = false;
-                }
-            }
-        }
-
-        return dp[n][m];
+     vector<vector<int>>dp(n,vector<int>(m+1,-1));
+        return f(n - 1, m - 1, s, p,dp);
     }
 };
