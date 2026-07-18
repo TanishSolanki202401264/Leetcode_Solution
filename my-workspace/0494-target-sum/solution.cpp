40afce1553bca6517a23212d559b6b3ef35c7e0f
Tanish Solanki <solankitanish77@gmail.com>
2026-07-18 12:27:08 +0000

    Sync LeetCode submission Runtime - 4 ms (77.78%), Memory - 15.6 MB (46.83%)

--- my-workspace/0494-target-sum/solution.cpp
+++ my-workspace/0494-target-sum/solution.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
 int f(int idx, int sum, vector<int>& arr, vector<vector<int>>& dp) {
-        if (idx == 0) {
-            if (sum == 0 && arr[0] == 0) return 2;
-            if (sum == 0 || sum == arr[0]) return 1;
+        if (idx==0) {
+            if (sum==0 && arr[0]==0) return 2;
+            if (sum==0||sum==arr[0]) return 1;
             return 0;
         }
         if (dp[idx][sum] != -1) {
             return dp[idx][sum];
         }
-        int nottake = f(idx - 1, sum, arr, dp);
+        int nottake = f(idx-1, sum, arr, dp);
 
-        int take = 0;
-        if (arr[idx] <= sum) {
-            take = f(idx - 1, sum - arr[idx], arr, dp);
+        int take =0;
+        if (arr[idx]<=sum) {
+            take = f(idx-1,sum-arr[idx],arr,dp);
         }
 
-        return dp[idx][sum] = take + nottake;
+        return dp[idx][sum]=take+nottake;
     }
     int findways(vector<int>& arr, int tar) {
         int n = arr.size();
-
-        vector<vector<int>> dp(n, vector<int>(tar + 1, -1));
-
-        return f(n - 1, tar, arr, dp);
+        vector<vector<int>> dp(n+1,vector<int>(tar+1,-1));
+        return f(n-1,tar,arr,dp);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
           int totalsum = 0;
@@ -33,6 +31,6 @@ int f(int idx, int sum, vector<int>& arr, vector<vector<int>>& dp) {
         if ((totalsum -target< 0) || (totalsum -target) % 2 != 0) {
             return 0;
         }
-        return findways(nums, (totalsum -target) / 2);
+        return findways(nums,(totalsum-target)/2);
     }
 };
