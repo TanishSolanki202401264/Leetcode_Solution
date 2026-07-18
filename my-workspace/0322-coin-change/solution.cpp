b752abe5f306a0120cc5c150f4416447a36f6719
Tanish Solanki <solankitanish77@gmail.com>
2026-07-18 11:41:45 +0000

    Sync LeetCode submission Runtime - 51 ms (25.70%), Memory - 53.7 MB (9.41%)

--- my-workspace/0322-coin-change/solution.cpp
+++ my-workspace/0322-coin-change/solution.cpp
@@ -23,23 +23,8 @@ public:
 
     int coinChange(vector<int>& coins, int amount) {
       int n=coins.size();
-      vector<vector<int>>dp(n,vector<int>(amount+1,0));
-      for(int T=0;T<=amount;T++){
-        if(T% coins[0]==0)
-        dp[0][T]=T/coins[0];
-        else  dp[0][T]=1e9;
-      }
-       for(int idx=1;idx<n;idx++){
-        for(int T=0;T<=amount;T++){
-            int nottake=0+dp[idx-1][T];
-            int take=INT_MAX;
-            if(coins[idx]<=T){
-                take=1+dp[idx][T-coins[idx]];
-            }
-            dp[idx][T]=min(take,nottake);
-        }
-       }
-      int ans=dp[n-1][amount];
+      vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+      int ans= f(n-1,amount,coins,dp);
       if(ans>=1e9){
         return -1;
       }
