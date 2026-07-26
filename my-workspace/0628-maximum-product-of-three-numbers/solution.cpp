4e54e0ee1c2214b183a93573fa56668632a0950b
Tanish Solanki <solankitanish77@gmail.com>
2026-07-26 12:11:49 +0000

    Sync LeetCode submission Runtime - 11 ms (35.07%), Memory - 31.4 MB (79.22%)

--- my-workspace/0628-maximum-product-of-three-numbers/solution.cpp
+++ my-workspace/0628-maximum-product-of-three-numbers/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-         int n=nums.size();
+        int n=nums.size();
         sort(nums.begin(),nums.end());
-        int ans1=nums[n-1]*nums[n-2]*nums[n-3];
-        int ans2=nums[0]*nums[1]*nums[n-1];
-        return max({ans1,ans2});
+        long long x= 1LL*nums[n-1]*nums[n-2]*nums[n-3];
+        long long y=1LL*nums[0]*nums[1]*nums[n-1];
+        return max(x,y);
      }
 };
