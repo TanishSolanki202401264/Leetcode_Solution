e1845b57f22ea20937eea527b794ed867115cebb
Tanish Solanki <solankitanish77@gmail.com>
2026-07-26 19:19:20 +0000

    Sync LeetCode submission Runtime - 4 ms (97.26%), Memory - 34.9 MB (59.64%)

---
+++ my-workspace/0542-01-matrix/solution.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>>dist(m,vector<int>(n,-1));
+        queue<pair<int,int>>q;
+        for(int i=0;i<m;i++){
+            //jeni jeni value zero hoy temne queue ma nakhii do pahele thii j and temni mate ans pan zero j aavse 
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    q.push({i,j});
+                    dist[i][j]=0;
+                }
+            }
+        }
+        int dr[]={-1,0,1,0};
+        int dc[]={0,1,0,-1};
+        while(!q.empty()){
+            auto cur=q.front();
+            q.pop();
+
+            int row=cur.first;
+            int col=cur.second;
+
+            for(int k=0;k<4;k++){
+                int nr=row+dr[k];
+                int nc=col+dc[k];
+
+                if(nr>=0 && nr<m && nc>=0 && nc<n && dist[nr][nc]==-1){
+                     dist[nr][nc]=dist[row][col]+1;
+                     q.push({nr,nc});
+                }
+                //jya sudhii dist[nr][nc]==-1 na rahe tya sudhii apde aa process mathii pass thavu padse 
+            }
+        }
+       return dist;
+    }
+};
